c9f48d8509cca0dfda026adce425e1329ab552e5
David L. Day <dday376@gmail.com>
2019-10-11 15:04:45 -0400

    added kafka setup script

---
+++ bank-balance/kafka-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# create input topic with one partition to get full ordering
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic bank-transactions
+
+# create output log compacted topic
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic bank-balance-exactly-once --config cleanup.policy=compact
+
+# launch a Kafka consumer
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 \
+    --topic account-balances \
+    --from-beginning \
+    --formatter kafka.tools.DefaultMessageFormatter \
+    --property print.key=true \
+    --property print.value=true \
+    --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+    --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
+
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 \
+    --topic bank-transactions \
+    --from-beginning \
+    --formatter kafka.tools.DefaultMessageFormatter \
+    --property print.key=true \
+    --property print.value=true \
+    --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+    --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer
