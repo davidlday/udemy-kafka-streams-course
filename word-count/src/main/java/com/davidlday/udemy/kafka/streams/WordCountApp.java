409028471c2ec77a7536f9a9b913a3c44ed2b0ab
David L. Day <dday376@gmail.com>
2019-10-13 21:15:49 -0400

    refactored for unit tests

--- word-count/src/main/java/com/davidlday/udemy/kafka/streams/WordCountApp.java
+++ word-count/src/main/java/com/davidlday/udemy/kafka/streams/WordCountApp.java
@@ -13,14 +13,7 @@ import java.util.Properties;
 
 public class WordCountApp {
 
-  public static void main(String[] args) {
-    Properties config = new Properties();
-    config.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-application");
-    config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-    config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-    config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-    config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-
+  public Topology createTopology() {
     StreamsBuilder builder = new StreamsBuilder();
     KStream<String, String> wordCountInput = builder.stream("word-count-input");
 
@@ -34,7 +27,20 @@ public class WordCountApp {
 
     wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
 
-    Topology topology = builder.build();
+    return builder.build();
+  }
+
+  public static void main(String[] args) {
+    Properties config = new Properties();
+    config.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-application");
+    config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+    WordCountApp wordCountApp = new WordCountApp();
+
+    Topology topology = wordCountApp.createTopology();
     KafkaStreams streams = new KafkaStreams(topology, config);
     streams.start();
 
