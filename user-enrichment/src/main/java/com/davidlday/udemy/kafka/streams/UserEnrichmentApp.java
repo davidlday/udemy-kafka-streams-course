eaa29cf3ecbc00a6138adbbc106677065c75ef15
David L. Day <dday376@gmail.com>
2019-10-13 23:23:27 -0400

    refactored for unit testing

--- user-enrichment/src/main/java/com/davidlday/udemy/kafka/streams/UserEnrichmentApp.java
+++ user-enrichment/src/main/java/com/davidlday/udemy/kafka/streams/UserEnrichmentApp.java
@@ -13,14 +13,7 @@ import java.util.Properties;
 
 public class UserEnrichmentApp {
 
-  public static void main(String[] args) {
-    Properties config = new Properties();
-    config.put(StreamsConfig.APPLICATION_ID_CONFIG, "user-enrichment-application");
-    config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-    config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-    config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-    config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-
+  public Topology createTopology() {
     StreamsBuilder streamsBuilder = new StreamsBuilder();
 
     GlobalKTable<String,String> users = streamsBuilder.globalTable("user-table");
@@ -48,8 +41,21 @@ public class UserEnrichmentApp {
     );
     userPurchaseLeftJoin.to("user-purchases-left-join");
 
+    return streamsBuilder.build();
+  }
+
+  public static void main(String[] args) {
+    Properties config = new Properties();
+    config.put(StreamsConfig.APPLICATION_ID_CONFIG, "user-enrichment-application");
+    config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+    UserEnrichmentApp userEnrichmentApp = new UserEnrichmentApp();
+
     // Create the streams
-    Topology topology = streamsBuilder.build();
+    Topology topology = userEnrichmentApp.createTopology();
     KafkaStreams streams = new KafkaStreams(topology, config);
     streams.start();
 
